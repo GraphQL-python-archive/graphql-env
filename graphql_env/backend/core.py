d82c02c4a82486c69a1a2fa9c262d74f335bdf26
Syrus Akbary <me@syrusakbary.com>
2018-02-02 10:41:09 -0800

    Improved GraphQLCoreBackend

--- graphql_env/backend/core.py
+++ graphql_env/backend/core.py
@@ -6,8 +6,12 @@ from .base import GraphQLBackend, GraphQLDocument
 
 
 class GraphQLCoreBackend(GraphQLBackend):
+    def __init__(self, executor=None, **kwargs):
+        super(GraphQLCoreBackend, self).__init__(**kwargs)
+        self.execute_params = {'executor': executor}
+
     def document_from_string(self, schema, request_string):
-        return GraphQLCoreDocument(schema, request_string)
+        return GraphQLCoreDocument(schema, request_string, self.execute_params)
 
 
 def get_operation_from_operation_name(document_ast, operation_name):
@@ -21,9 +25,10 @@ def get_operation_from_operation_name(document_ast, operation_name):
 class GraphQLCoreDocument(GraphQLDocument):
     errors = None
 
-    def __init__(self, schema, request_string):
+    def __init__(self, schema, request_string, execute_params=None):
         self.schema = schema
         self.request_string = request_string
+        self.execute_params = execute_params or {}
         try:
             self.document_ast = parse(request_string)
             validation_errors = validate(schema, self.document_ast)
@@ -36,26 +41,27 @@ class GraphQLCoreDocument(GraphQLDocument):
     def execute(self,
                 root=None,
                 context=None,
+                middleware=None,
                 operation_name=None,
                 variables=None,
-                allowed_operations=None,
-                **extra):
+                allowed_operations=None):
         if self.errors:
             return ExecutionResult(errors=self.errors, invalid=True)
         try:
-            operation_type = get_operation_from_operation_name(
-                self.document_ast, operation_name)
-            # raise Exception(operation_name)
-            if operation_type and operation_type not in allowed_operations:
-                raise GraphQLError(
-                    "{} operations are not allowed.".format(operation_type))
+            if allowed_operations is not None:
+                operation_type = get_operation_from_operation_name(
+                    self.document_ast, operation_name)
+                if operation_type and operation_type not in allowed_operations:
+                    raise GraphQLError("{} operations are not allowed.".format(
+                        operation_type))
             return execute(
                 self.schema,
                 self.document_ast,
                 root_value=root,
+                middleware=middleware,
                 context_value=context,
-                variable_values=variables,
+                variable_values=variables or {},
                 operation_name=operation_name,
-                **extra)
+                **self.execute_params)
         except Exception as e:
             return ExecutionResult(errors=[e])
--- tests/test_graphql_environment.py
+++ tests/test_graphql_environment.py
@@ -5,6 +5,7 @@
 import pytest
 
 from graphql_env import GraphQLEnv, GraphQLBackend, GraphQLCoreBackend, GraphQLDocument, GraphQLDeciderBackend
+from graphql.execution.executors.sync import SyncExecutor
 from .schema import schema
 
 
@@ -38,6 +39,33 @@ def test_backend_will_compute_if_cache_non_existing():
     document1 = graphql_env.document_from_string('{ hello }')
     document2 = graphql_env.document_from_string('{ hello }')
     assert document1 != document2
+
+
+def test_backend_can_execute():
+    graphql_env = GraphQLEnv(schema=schema, backend=GraphQLCoreBackend())
+    document1 = graphql_env.document_from_string('{ hello }')
+    result = document1.execute()
+    assert not result.errors
+    assert result.data == {'hello': 'World'}
+
+
+class BaseExecutor(SyncExecutor):
+    executed = False
+
+    def execute(self, *args, **kwargs):
+        self.executed = True
+        return super(BaseExecutor, self).execute(*args, **kwargs)
+
+
+def test_backend_can_execute_custom_executor():
+    executor = BaseExecutor()
+    graphql_env = GraphQLEnv(
+        schema=schema, backend=GraphQLCoreBackend(executor=executor))
+    document1 = graphql_env.document_from_string('{ hello }')
+    result = document1.execute()
+    assert not result.errors
+    assert result.data == {'hello': 'World'}
+    assert executor.executed
 
 
 class FakeBackend(GraphQLBackend):
